d154eef75337355a3e07a3db6d491705eab9c766
Artem134134 <car322132@gmail.com>
2022-05-10 20:16:07 +0300

    In QuestionController add method 'update'

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,5 +6,14 @@ class QuestionsController < ApplicationController
   	  body: params[:question][:body],
   	  user_id: params[:question][:user_id]	
   		)
+
+  end
+
+  def update 
+  	@question = Question.find(params[:id])
+  	@question.update(
+  	  body: params[:question][:body],
+  	  user_id: params[:question][:user_id]
+  	)
   end
 end
