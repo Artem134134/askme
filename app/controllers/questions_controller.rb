9744438e99eac56919d00249c0b91bd4104d63a8
Artem134134 <car322132@gmail.com>
2022-05-10 16:53:55 +0300

    Add in question controller,params[body and user_id]

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,10 @@
 class QuestionsController < ApplicationController
-	def create
-	end
+  skip_before_action :verify_authenticity_token
+
+  def create 
+  	Question.create(
+  	  body: params[:question][:body],
+  	  user_id: params[:question][:user_id]	
+  		)
+  end
 end
