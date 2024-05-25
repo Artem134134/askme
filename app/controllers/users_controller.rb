46611f9508219d923be7f258e0a81c0088367dc2
Artem134134 <car322132@gmail.com>
2024-05-25 17:28:53 +0300

    Added parameter checking to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,15 @@ class UsersController < ApplicationController
   def create
     user_params = params.require(:user).permit(:name,:nickname,:email, :password)
   
-    User.create(user_params)
+    @user = User.new(user_params)
 
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+      if @user.save
+        redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+      else
+        flash.now[:alert] = 'Вы неправильно заполнили поля формы регистрации'
+      
+        render :new
+      end
+
   end
 end
