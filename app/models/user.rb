7360783e4dfeaa3bd321b9807aa6c1c58f9925c5
Artem134134 <car322132@gmail.com>
2024-05-25 21:03:03 +0300

    A method has been written for the nickname parameter to convert to lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
 	has_secure_password # тоже проверяет на присутствие.
 
+	before_save :downcase_nickname
+	
   validates :email, presence: true, uniqueness: {case_sensitive: false}
 
+  def downcase_nickname
+  	nickname.downcase!
+  end
 end
