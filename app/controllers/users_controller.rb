d0162b8b0705ba653662700b981cd1fd2b06dcff
Artem134134 <car322132@gmail.com>
2024-05-25 20:14:32 +0300

    added parameter password_confirmation to users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name,:nickname,:email, :password)
+    user_params = params.require(:user).permit(
+      :name,:nickname,
+      :email, :password,
+      :password_confirmation
+      )
   
     @user = User.new(user_params)
 
