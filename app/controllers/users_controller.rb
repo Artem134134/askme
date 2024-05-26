71e350cf267118be3be952b0f5c7166fc6ea640e
Artem134134 <car322132@gmail.com>
2024-05-26 14:23:13 +0300

    An update method was written for the user in the controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id]) 
   end
 
+  def update
+    @user = User.find(params[:id]) 
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'Данные пользователя успешно обновлены.'
+    else
+      flash.now[:alert] = 'При попытке сохранить пользователя возникла ошибка!'      
+      render :edit 
+    end
+
+  end
+
   private
 
   def user_params
