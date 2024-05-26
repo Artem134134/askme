f62584022a11e6e1ba19b8d1beffd42451e72b07
Artem134134 <car322132@gmail.com>
2024-05-26 13:41:38 +0300

    In users_controller.rb was been method edit. Parameters were moved to the private user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,21 +5,27 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(
-      :name,:nickname,
-      :email, :password,
-      :password_confirmation
-      )
-  
     @user = User.new(user_params)
 
-      if @user.save
-        redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
-      else
-        flash.now[:alert] = 'Вы неправильно заполнили поля формы регистрации'
-      
-        render :new 
-      end
-
+    if @user.save
+      redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    else
+      flash.now[:alert] = 'Вы неправильно заполнили поля формы регистрации'      
+      render :new 
+    end
   end
+
+  def edit
+    @user = User.find(params[:id]) 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,:nickname,
+      :email, :password,
+      :password_confirmation)
+  end
+
 end
